a97746fc26b63f1cd7017353871a4670c04534de
rkapl123 <rolandk@kapl.org>
2017-05-20 20:49:03 +0200

    compiled 64 bit version using 64 bit Excel added all used packages (exceldna.addin, exceldna.integration, exceldna.interop, rdotnet, dynamicinterop) in repository

--- RAddin.vb
+++ RAddin.vb
@@ -4,21 +4,25 @@ Imports System.IO
 Imports System.Runtime.InteropServices
 Imports System.Configuration
 Imports ExcelDna.Integration.CustomUI
+Imports RDotNet
 
-Public Module MyFunctions
+Public Module RAddin
+
+    Public _engine As REngine
     Public currWb As Workbook
     Public Rdefinitions As Range
     Public Rcalldefnames As String() = {}
     Public Rcalldefs As Range() = {}
     Public theRibbon As ExcelDna.Integration.CustomUI.IRibbonUI
     Public rexec As String
+    Public rpath As String
     Dim dirglobal As String
 
     ' definitions of current R invocation (scripts, args, results, diags...)
     Dim RdefDic As Dictionary(Of String, String()) = New Dictionary(Of String, String())
 
     ' prepare Parameters (script, args, results, diags) for usage in invokeScripts, storeArgs, getResults and getDiags 
-    Function prepareParams(c As Integer, name As String, ByRef RDataRange As Range, ByRef returnName As String, ByRef returnPath As String, ext As String) As String
+    Private Function prepareParams(c As Integer, name As String, ByRef RDataRange As Range, ByRef returnName As String, ByRef returnPath As String, ext As String) As String
         Dim value As String = RdefDic(name)(c)
         ' only for args, results and diags (scripts dont have a target range)
         If name = "args" Or name = "results" Or name = "diags" Or name = "scriptrng" Then
@@ -100,7 +104,7 @@ Public Module MyFunctions
     End Function
 
     ' creates script files for defined scriptRng ranges 
-    Public Function storeScriptRng() As String
+    Private Function storeScriptRng() As String
         Dim scriptRngFilename As String = vbNullString, scriptRngdir = vbNullString, scriptText = vbNullString
         Dim RDataRange As Range = Nothing
         Dim outputFile As StreamWriter = Nothing
@@ -140,7 +144,7 @@ Public Module MyFunctions
                     Do
                         Dim j As Integer = 1
                         Dim writtenLine As String = ""
-                        If RDataRange(i, 1).Value2.ToString <> "" Then
+                        If RDataRange(i, 1).Value2 IsNot Nothing Then
                             Do
                                 writtenLine = writtenLine + RDataRange(i, j).Value2
                                 j = j + 1
@@ -160,7 +164,7 @@ Public Module MyFunctions
     End Function
 
     ' invokes current scripts/args/results definition
-    Public Function invokeScripts() As String
+    Private Function invokeScripts() As String
         Dim script As String = vbNullString
         Dim scriptpath As String
 
@@ -203,7 +207,7 @@ Public Module MyFunctions
 
     ' get Outputfiles for defined results ranges, tab separated
     ' otherwise:  "what you see is what you get"
-    Public Function getResults() As String
+    Private Function getResults() As String
         Dim resFilename As String = vbNullString, readdir As String
         Dim RDataRange As Range = Nothing
         Dim errMsg As String = vbNullString
@@ -250,7 +254,7 @@ Public Module MyFunctions
     End Function
 
     ' get Output diagrams (png) for defined diags ranges
-    Public Function getDiags() As String
+    Private Function getDiags() As String
         Dim diagFilename As String = vbNullString, readdir As String
         Dim RDataRange As Range = Nothing
         Dim errMsg As String = vbNullString
@@ -271,7 +275,7 @@ Public Module MyFunctions
             ' absolute paths begin with \\ or X:\ -> dont prefix with currWB path, else currWBpath\readdir
             Dim curWbPrefix As String = IIf(Left(readdir, 2) = "\\" Or Mid(readdir, 2, 2) = ":\", "", currWb.Path + "\")
             Try
-                With RDataRange.Worksheet.Shapes.AddPicture(Filename:=curWbPrefix + readdir + "\" + diagFilename, _
+                With RDataRange.Worksheet.Shapes.AddPicture(Filename:=curWbPrefix + readdir + "\" + diagFilename,
                     LinkToFile:=False, SaveWithDocument:=True, Left:=RDataRange.Left, Top:=RDataRange.Top, Width:=-1, Height:=-1)
                     .Name = diagFilename
                 End With
@@ -283,7 +287,7 @@ Public Module MyFunctions
     End Function
 
     ' remove result, diagram and temporary R script files
-    Public Function removeFiles() As String
+    Private Function removeFiles() As String
         Dim filename As String = vbNullString, readdir As String
         Dim RDataRange As Range = Nothing
         Dim errMsg As String = vbNullString
@@ -351,25 +355,56 @@ Public Module MyFunctions
         ' get the definition range
         errStr = getRDefinitions()
         If errStr <> vbNullString Then Return "Failed getting Rdefinitions: " + errStr
-        ' remove result and diagram files from arg(ument) ranges
-        errStr = removeFiles()
-        If errStr <> vbNullString Then Return "removing files returned error: " + errStr
-        ' store input files from arg(ument) ranges
-        errStr = storeArgs()
-        If errStr <> vbNullString Then Return "storing input files returned error: " + errStr
-        ' store scripts contained in Range from scriptRng ranges
-        errStr = storeScriptRng()
-        If errStr <> vbNullString Then Return "storing scriptRng ranges returned error: " + errStr
-        ' invoke r script(s)
-        errStr = invokeScripts()
-        If errStr <> vbNullString Then Return "invoking scripts returned error: " + errStr
-        ' get and write output files into res(ult) ranges
-        errStr = getResults()
-        If errStr <> vbNullString Then Return "fetching/placing result files/content returned error: " + errStr
-        ' get and put result diagrams/pictures into dia(gram) ranges
-        errStr = getDiags()
-        If errStr <> vbNullString Then Return "fetching/placing result diagrams returned error: " + errStr
-        ' all ís OK = return nullstring
+        If rexec IsNot Nothing And rpath Is Nothing Then ' shell invocation
+            ' remove result and diagram files from arg(ument) ranges
+            errStr = removeFiles()
+            If errStr <> vbNullString Then Return "removing files returned error: " + errStr
+            ' store input files from arg(ument) ranges
+            errStr = storeArgs()
+            If errStr <> vbNullString Then Return "storing input files returned error: " + errStr
+            ' store scripts contained in Range from scriptRng ranges
+            errStr = storeScriptRng()
+            If errStr <> vbNullString Then Return "storing scriptRng ranges returned error: " + errStr
+            ' invoke r script(s)
+            errStr = invokeScripts()
+            If errStr <> vbNullString Then Return "invoking scripts returned error: " + errStr
+            ' get and write output files into res(ult) ranges
+            errStr = getResults()
+            If errStr <> vbNullString Then Return "fetching/placing result files/content returned error: " + errStr
+            ' get and put result diagrams/pictures into dia(gram) ranges
+            errStr = getDiags()
+            If errStr <> vbNullString Then Return "fetching/placing result diagrams returned error: " + errStr
+        ElseIf rpath IsNot Nothing Then ' RDotNet invocation
+            ' initialize RdotNet engine
+            errStr = InitializeRDotNet()
+            If errStr <> vbNullString Then Return "initializing RdotNet returned error: " + errStr
+            ' test RdotNet engine
+            errStr = TestRDotNet()
+            If errStr <> vbNullString Then Return "TestRDotNet returned error: " + errStr
+        End If
+        ' all is OK = return nullstring
+        Return vbNullString
+    End Function
+
+    Private Function InitializeRDotNet() As String
+        Try
+            REngine.SetEnvironmentVariables(rPath:=rpath)
+            _engine = REngine.GetInstance()
+            _engine.Initialize()
+        Catch ex As Exception
+            Return "Error initializing RDotNet: " + ex.Message
+        End Try
+        Return vbNullString
+    End Function
+
+    Private Function TestRDotNet() As String
+        Dim theArray() As Double = {30.02, 29.99, 30.11, 29.97, 30.01, 29.99}
+        Dim group1 As NumericVector = _engine.CreateNumericVector(theArray)
+        _engine.SetSymbol("group1", group1)
+        Dim group2 As NumericVector = _engine.Evaluate("group2 <- c(29.89, 29.93, 29.72, 29.98, 30.02, 29.98)").AsNumeric()
+        ' Test difference of mean And get the P-value.
+        Dim testResult As GenericVector = _engine.Evaluate("t.test(group1, group2)").AsList()
+        Dim p As Double = testResult("p.value").AsNumeric().First()
         Return vbNullString
     End Function
 
@@ -384,19 +419,12 @@ Public Module MyFunctions
         ElseIf errStr <> vbNullString Then
             Return "Error while getRNames in startRdefRefresh: " + errStr
         End If
-        MyFunctions.theRibbon.Invalidate()
+        RAddin.theRibbon.Invalidate()
         Return vbNullString
     End Function
 
     ' gets defined named ranges for R script invocation in the current workbook 
-    Function getRNames() As String
-        ' get default rexec path from user (or overriden in appSettings tag as redirect to global) settings. This can be overruled by individual rexec settings in Rdefinitions
-        Try
-            rexec = ConfigurationManager.AppSettings("ExePath").ToString()
-        Catch ex As Exception
-            Return "no ExePath when retrieving AppSettings in getRNames: " + ex.Message
-        End Try
-
+    Public Function getRNames() As String
         ReDim Preserve Rcalldefnames(-1)
         ReDim Preserve Rcalldefs(-1)
         For Each namedrange As Name In currWb.Names
@@ -421,7 +449,7 @@ Public Module MyFunctions
     End Function
 
     ' gets definitions from  current selected R script invocation range (Rdefintions)
-    Function getRDefinitions() As String
+    Public Function getRDefinitions() As String
         Try
             RdefDic("args") = {}
             RdefDic("argspaths") = {}
@@ -435,13 +463,16 @@ Public Module MyFunctions
             RdefDic("scriptrngpaths") = {}
             RdefDic("debugrng") = {}
             RdefDic("debug") = {}
+            rpath = Nothing : rexec = Nothing : dirglobal = vbNullString
             For Each defRow As Range In Rdefinitions.Rows
                 Dim deftype As String, defval As String, deffilepath As String
                 deftype = LCase(defRow.Cells(1, 1).Value2)
                 defval = defRow.Cells(1, 2).Value2
                 deffilepath = defRow.Cells(1, 3).Value2
-                If deftype = "rexec" Then
+                If deftype = "rexec" Then ' setting for shell innvocation
                     rexec = defval
+                ElseIf deftype = "rpath" Then ' setting for RdotNet
+                    rpath = defval
                 ElseIf deftype = "arg" Then
                     ReDim Preserve RdefDic("args")(RdefDic("args").Length)
                     RdefDic("args")(RdefDic("args").Length - 1) = defval
@@ -475,13 +506,24 @@ Public Module MyFunctions
                     dirglobal = defval
                 End If
             Next
-            If rexec = "" Then Return "Error in getRDefinitions: no rexec defined"
+            ' get default rexec path from user (or overriden in appSettings tag as redirect to global) settings. This can be overruled by individual rexec settings in Rdefinitions
+            Try
+                If rexec Is Nothing Then rexec = ConfigurationManager.AppSettings("ExePath").ToString()
+            Catch ex As Exception
+            End Try
+            ' get default Rpath from user (or overriden in appSettings tag as redirect to global) settings. This can be overruled by individual rexec settings in Rdefinitions
+            Try
+                If rpath Is Nothing Then rpath = ConfigurationManager.AppSettings("RPath").ToString()
+            Catch ex As Exception
+            End Try
+            If rexec Is Nothing And rpath Is Nothing Then Return "Error in getRDefinitions: neither rexec nor rpath defined"
             If RdefDic("scripts").Count = 0 And RdefDic("scriptrng").Count = 0 Then Return "Error in getRDefinitions: no script(s) or scriptRng(s) defined"
         Catch ex As Exception
             Return "Error in getRDefinitions: " + ex.Message
         End Try
         Return vbNullString
     End Function
+
 End Module
 
 ' Events from Excel (Workbook_Save ...)
@@ -489,7 +531,6 @@ Public Class AddIn
     Implements IExcelAddIn
 
     WithEvents Application As Application
-    WithEvents Button As Microsoft.Office.Core.CommandBarButton
 
     ' connect to Excel when opening Addin
     Public Sub AutoOpen() Implements IExcelAddIn.AutoOpen
@@ -508,7 +549,7 @@ Public Class AddIn
             MsgBox("Error when getting definitions in Workbook_Save: " + errStr)
             Exit Sub
         End If
-        errStr = storeArgs()
+        errStr = RAddin.storeArgs()
         If errStr <> "" Then MsgBox("Error when saving inputfiles in Workbook_Save: " + errStr)
     End Sub
 
@@ -524,7 +565,7 @@ Public Class AddIn
             MsgBox("Error when getting definitions in Workbook_Activate: " + errStr)
             Exit Sub
         End If
-        MyFunctions.theRibbon.Invalidate()
+        RAddin.theRibbon.Invalidate()
     End Sub
 
     Private Function doDefinitions(Wb As Workbook) As String
@@ -533,13 +574,13 @@ Public Class AddIn
         ' always reset Rdefinitions when changing Workbooks (may not be the current one, if saved programmatically!), otherwise this is not being refilled in getRNames
         Rdefinitions = Nothing
         ' get the defined R_Addin Names
-        errStr = getRNames()
-        If errStr = "no RNames" Then Return "no RNames"
+        errStr = RAddin.getRNames()
+        If errStr = "no RNames" Then Return errStr
         If errStr <> vbNullString Then
             Return "Error while getRNames in doDefinitions: " + errStr
         End If
         ' get the definitions from the current defined range (first name in R_Addin Names)
-        errStr = getRDefinitions()
+        errStr = RAddin.getRDefinitions()
         If errStr <> vbNullString Then Return "Error while getRdefinitions in doDefinitions: " + errStr
         Return vbNullString
     End Function
@@ -556,18 +597,18 @@ Public Class MyRibbon
             MsgBox("no Rdefinitions found for R_Addin in current Workbook (3 column named range (type/value/path), minimum types: rexec and script)!")
             Exit Sub
         End If
-        If MyFunctions.Rdefinitions Is Nothing Then
+        If RAddin.Rdefinitions Is Nothing Then
             MsgBox("Rdefinitions Is Nothing (this shouldn't actually happen) !")
             Exit Sub
         End If
-        errStr = MyFunctions.startRprocess()
+        errStr = RAddin.startRprocess()
         If errStr <> "" Then MsgBox(errStr)
     End Sub
 
 
     Public Sub refreshRdefs(control As ExcelDna.Integration.CustomUI.IRibbonControl)
         Dim errStr As String
-        errStr = MyFunctions.startRdefRefresh()
+        errStr = RAddin.startRdefRefresh()
         If errStr <> vbNullString Then
             MsgBox(errStr)
         Else
@@ -576,24 +617,24 @@ Public Class MyRibbon
     End Sub
 
     Public Function GetItemCount(control As ExcelDna.Integration.CustomUI.IRibbonControl) As Integer
-        Return (MyFunctions.Rcalldefnames.Length)
+        Return (RAddin.Rcalldefnames.Length)
     End Function
 
     Public Function GetItemLabel(control As ExcelDna.Integration.CustomUI.IRibbonControl, index As Integer) As String
-        Return MyFunctions.Rcalldefnames(index)
+        Return RAddin.Rcalldefnames(index)
     End Function
 
     Public Function GetItemID(control As ExcelDna.Integration.CustomUI.IRibbonControl, index As Integer) As String
-        Return MyFunctions.Rcalldefnames(index)
+        Return RAddin.Rcalldefnames(index)
     End Function
 
     Public Sub selectItem(control As ExcelDna.Integration.CustomUI.IRibbonControl, id As String, index As Integer)
-        MyFunctions.Rdefinitions = Rcalldefs(index)
-        MyFunctions.Rdefinitions.Parent.Select()
-        MyFunctions.Rdefinitions.Select()
+        RAddin.Rdefinitions = Rcalldefs(index)
+        RAddin.Rdefinitions.Parent.Select()
+        RAddin.Rdefinitions.Select()
     End Sub
     Public Sub ribbonLoaded(myribbon As ExcelDna.Integration.CustomUI.IRibbonUI)
-        MyFunctions.theRibbon = myribbon
+        RAddin.theRibbon = myribbon
     End Sub
 
 End Class
