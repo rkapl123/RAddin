bb55a2da356ec0aa139a19f69fdae34a591ec460
Roland Kapl <rkapl123@users.noreply.github.com>
2019-05-29 10:05:09 +0200

    removed MRO Registry search, set RPath directly

--- test/testStartRscript.cmd
+++ test/testStartRscript.cmd
@@ -1,8 +1,3 @@
-@echo OFF
-setlocal ENABLEEXTENSIONS
-FOR /F "usebackq skip=2 tokens=1 delims=" %%A IN (`REG QUERY HKLM\Software\R-core\R /v InstallPath 2^>nul`) DO (
-    set result=%%A
-)
-SET rpath=%result:*REG_SZ    =%
+SET rpath=\\oebfacoat\dfs\SOFTWARE\TRIT\R\R-3.4.1
 "%rpath%\bin\Rscript.exe" %~dp0\TestScript.R
 pause
